65dbd5d4349e0d4a1b71741136cde3dd9e264eb6
asapwonkie <brendomchugh82@gmail.com>
2020-03-08 11:43:03 -0400

    2020-3-8

--- Dungeon Crawler/Main.gd
+++ Dungeon Crawler/Main.gd
@@ -3,13 +3,12 @@
 class_name Main
 extends Node
 
-const GameObject = preload("res://GameObject.gd")
-const Health = preload("res://Health.gd")
-const AIRandomMovement = preload("res://AIRandomMovement.gd")
-
 const PlayerScene = preload("res://newplayerscenetest.tscn")
 
 func _ready():
+	spawn_player_circle(Vector2(130, 120))
+		
+func spawn_player_circle(origin: Vector2) -> void:
 	var radius = 10
 	var phi = 0
 	var amount_of_circles = 3
@@ -18,16 +17,13 @@ func _ready():
 	while amount_of_circles > 0:
 		phi = 0
 		while phi < 2 * PI * (1 - 1/amount_in_circle):
-			spawn_player(radius * Vector2(cos(phi), sin(phi)) + Vector2(100, 100))
+			spawn_player(radius * Vector2(cos(phi), sin(phi)) + origin)
 			phi +=  2 * PI / amount_in_circle
 		radius += 30
 		amount_in_circle += 8
 		amount_of_circles -= 1
 
-#	spawn_player(50 * Vector2(1, 0) + Vector2(100, 100))
-#	spawn_player(50 * Vector2(0, -1) + Vector2(100, 100))
-
-func spawn_player(var position: Vector2):
+func spawn_player(var position: Vector2) -> void:
 	var player = PlayerScene.instance()
 	player.get_node("KinematicBody2D").transform.origin = position
 	add_child(player)
--- Dungeon Crawler/Spin.gd
+++ Dungeon Crawler/Spin.gd
@@ -8,6 +8,12 @@ onready var transform_holder = get_node(transform_holder_path)
 
 var time = 0
 var spin_speed = 7
+var time_offset = 0
+
+func _ready():
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	time_offset = rng.randf_range(0,1)
 
 func _physics_process(delta):
 	time += delta
@@ -16,8 +22,9 @@ func _physics_process(delta):
 #	transform_holder.transform.x = Vector2(cos(spin_speed * time), 0)
 #	transform_holder.transform.y = Vector2(0, sin(spin_speed * time))
 
-	transform_holder.transform.x = Vector2(sin(PI * time / 0.2) + 1.5, 0)
-	transform_holder.transform.y = Vector2(0, sin(PI * time / 0.2) + 1.5)
+#	stretch
+#	transform_holder.transform.x = Vector2(sin(PI * time / 0.2) + 1.5, 0)
+#	transform_holder.transform.y = Vector2(0, sin(PI * time / 0.2) + 1.5)
 
-#	transform_holder.transform.x = Vector2(0.5 * (1 + cos(spin_speed * time)), 0)
-#	transform_holder.transform.y = Vector2(0, 1)
+	transform_holder.transform.x = Vector2(0.5 * (1 + cos(spin_speed * (time + time_offset))), 0)
+	transform_holder.transform.y = Vector2(0, 1)
